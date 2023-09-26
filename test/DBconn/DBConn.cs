7d6aea4fd6111782e1dd51c705b2f0bae5ac8c34
qwer7824 <sh7824@naver.com>
2023-09-26 19:02:59 +0900

    DBconn
    properties 로 db 세팅

---
+++ test/DBconn/DBConn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace test.DBConn
+{
+    class DBConn
+    {
+        private static string DBIP = Properties.Settings.Default.DBIP;
+        private static int DBPORT = Properties.Settings.Default.DBPORT;
+        private static string DB = Properties.Settings.Default.DB;
+        private static string DBID = Properties.Settings.Default.DBID;
+        private static string DBPW = Properties.Settings.Default.DBPW;
+        private static MySqlConnection conn = null;
+
+        public static MySqlConnection getConn()
+        {
+            string connString = "Server=" + DBIP
+                                 + ";Port=" + DBPORT
+                                  + ";Database=" + DB
+                                  + ";Uid=" + DBID
+                                    + ";Pwd=" + DBPW;
+
+            conn = new MySqlConnection(connString);
+
+            return conn;
+        }
+    }
+}
